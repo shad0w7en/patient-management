b8818d03dc45689db5ca30ab2ad08de226afbf28
shad0w7en <ankuryadav93932@gmail.com>
2025-06-28 17:11:48 +0000

    added Integration Test

---
+++ patient-management/integration-tests/src/test/java/AuthIntegrationTest.java
@@ -0,0 +1,61 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class AuthIntegrationTest {
+    @BeforeAll
+    static void setUp(){
+        RestAssured.baseURI = "http://localhost:4004";
+    }
+
+    @Test
+    public void shouldReturnOkWithValidToken(){
+        //Arrange
+        //ACT
+        //Assert
+        String loginPayload ="""
+                {
+                "email" :"testuser@test.com",
+                "password" :"password123"
+                }
+             """;
+
+
+        Response response = given()
+                .contentType("application/json")
+                .body(loginPayload)
+                .post("/api/v1/auth/login")
+                .then()
+                .statusCode(200)
+                .body("token", notNullValue())
+                .extract()
+                .response();
+
+        System.out.println("Generated Token" +response.jsonPath().getString("token"));
+    }
+
+
+    @Test
+    public void shouldReturnUnauthorizedOnInvalidLogin(){
+        String loginPayload ="""
+                {
+                "email" :"testuser@test.com",
+                "password" :"wrongpassword"
+                }
+             """;
+
+
+        Response response = given()
+                .contentType("application/json")
+                .body(loginPayload)
+                .post("/api/v1/auth/login")
+                .then()
+                .statusCode(401)
+                .extract()
+                .response();
+    }
+}
